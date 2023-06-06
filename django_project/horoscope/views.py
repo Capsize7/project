2f65c0e4a8337a983c31179012521464ebca78af
Capsize7 <dnbforever7@gmail.com>
2023-06-06 12:10:26 +0300

    Add some sign of horoscope

--- django_project/horoscope/views.py
+++ django_project/horoscope/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def leo(request):
+    return HttpResponse("Знак зодиака - Лев")
+
+def scorpio(request):
+    return HttpResponse("Знак зодиака - Скорпион")
+
+
+def aries(request):
+    return HttpResponse("Знак зодиака - Овен")
+
+def capricorn(request):
+    return HttpResponse("Знак зодиака - Козерог")

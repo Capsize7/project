ade1a2d1bdca5feea5145ab0b1d6559a1d4fbeb9
Capsize7 <dnbforever7@gmail.com>
2023-06-06 13:56:26 +0300

    Add debug config for server

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('horoscope/', include('horoscope.urls')),
-    path('week_days/', include('week_days.urls'))
+    path('todo_week/', include('week_days.urls'))
 ]
--- django_project/week_days/urls.py
+++ django_project/week_days/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from week_days import views as wdv
 urlpatterns = [
-    path('todo_week/monday/', wdv.monday),
-    path('todo_week/tuesday/', wdv.tuesday)
+    path('monday/', wdv.monday),
+    path('tuesday/', wdv.tuesday)
 ]
